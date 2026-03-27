46d5ec120b2881e677c3b8e21c841a91ac47134d
ILeonardo-Lima <leonardolimaoliveira5@gmail.com>
2026-03-27 03:51:56 -0300

    Corrigindo duplicidade do cors e ordem do app

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const pool = new Pool({
 pool
   .connect()
   .then(() => console.log("✅ Conectado ao PostgreSQL com sucesso!"))
-  .catch((err) => console.error("❌ Erro ao conectar:", err.message));
+  .catch((err) => console.error("❌ Erro ao conectar::", err.message));
 
 // ====================== ROTAS ======================
 
