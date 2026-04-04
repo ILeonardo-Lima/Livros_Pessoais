afa008e4f669d28daa3737ed34d55fe4a96f8102
ILeonardo-Lima <leonardolimaoliveira5@gmail.com>
2026-04-04 02:29:44 -0300

    Atulização na header para mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -296,13 +296,13 @@ export default function App() {
       <div
         className={`min-h-screen transition-colors duration-500 ${darkMode ? "bg-black" : "bg-slate-50"}`}
       >
-        <nav className="bg-zinc-800 text-white py-4 px-6 md:px-12 flex justify-between items-center sticky top-0 z-50 shadow-2xl">
+        <nav className="bg-zinc-800 text-white py-2 px-4 md:py-4 md:px-12 flex justify-between items-center sticky top-0 z-50 shadow-2xl">
           <div className="flex items-center gap-2">
             <div className="bg-indigo-600 p-2 rounded-lg">
               <BookOpen size={24} />
             </div>
 
-            <h1 className="text-xl font-black tracking-tighter">
+            <h1 className="text-lg sm:text-2xl font-black leading-tight">
               MINHA ESTANTE
             </h1>
           </div>
@@ -312,9 +312,9 @@ export default function App() {
           <div className="flex items-center gap-4">
             <button
               onClick={() => setDarkMode(!darkMode)}
-              className={`p-2.5 rounded-xl transition-all ${darkMode ? "bg-zinc-800 text-yellow-400" : "bg-zinc-800 text-gray-400 hover:text-white"}`}
+              className={`p-1.5 sm:p-2.5 rounded-xl bg-zinc-700 hover:bg-zinc-600 transition-colors shadow-lg ${darkMode ? "bg-zinc-800 text-yellow-400" : "bg-zinc-800 text-gray-400 hover:text-white"}`}
             >
-              {darkMode ? <Sun size={20} /> : <Moon size={20} />}
+              {darkMode ? <Sun size={18} /> : <Moon size={18} />}
             </button>
 
             <button
