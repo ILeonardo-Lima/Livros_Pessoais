db8e1b07fef2ca4c359eaf251bfc4c9302950b36
ILeonardo-Lima <leonardolimaoliveira5@gmail.com>
2026-04-07 01:05:58 -0300

    Linha removida definitivamente

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -529,7 +529,7 @@ export default function App() {
                         }`}
                       >
                         <p
-                          className={`font-bold mt-3 border-t  pt-2 ${
+                          className={`font-bold mt-3 ${
                             livro.status === "Lido"
                               ? "text-green-500"
                               : livro.status === "Lendo"
